71c2fdfa4781890a7dc2ccb99634703e3fa36ec4
Daniel Petti <djpetti@gmail.com>
2017-02-03 11:00:22 -0500

    Add code for pretraining.
    This will allow us to test with a network more like what they
    used in the paper.

--- train_action.py
+++ train_action.py
@@ -46,10 +46,10 @@ import action_predict
 logger = logging.getLogger(__name__)
 
 
-batch_size = 128
+batch_size = 256
 eval_batch_size = 150
 # How many batches to have loaded into VRAM at once.
-load_batches = 5
+load_batches = 4
 # How many batches to have loaded into VRAM at once when evaluating.
 eval_load_batches = 4
 # Shape of the input images.
@@ -66,15 +66,25 @@ weight_decay = 0.00005
 decay_rate = 0.85
 decay_steps = 10000
 
+# Learning rate exponential decay hyperparameters for pretraining.
+pretrain_decay_rate = 0.1
+pretrain_decay_steps = 80000
+
 # Where to save the network.
 save_file = "/job_files/action_frame_alexnet_split_1.pkl"
-# Location of the original file that we should start training from.
-alexnet_file = "/training_data/rpinets/alexnet.pkl"
 synsets_save_file = "/job_files/synsets.pkl"
+# Location of the original file that we should start training from.
+alexnet_file = "/training_data/action/alexnet_m_2048.pkl"
+alexnet_synsets_file = "/training_data/rpinets/synsets.pkl"
 # Location of the dataset files.
 dataset_files = "/training_data/action/split1_data/dataset"
+alexnet_dataset_files = "/training_data/rpinets/ilsvrc16_dataset"
 # Location of the cache files.
 cache_dir = "/training_data/action/split1_data/cache"
+alexnet_cache_dir = "/training_data/rpinets/cache"
+# Location of synset data.
+synset_location = "/training_data/rpinets/synsets"
+synset_list = "/job_files/ilsvrc_synsets.txt"
 
 
 def train():
@@ -101,9 +111,6 @@ def train():
   else:
     # Load the pretrained AlexNet and work off of that.
     logger.info("Loading pretrained AlexNet '%s'..." % (alexnet_file))
-    if not os.path.exists(alexnet_file):
-      logger.critical("AlexNet file '%s' not found!" % (alexnet_file))
-      sys.exit(1)
 
     # Setup exponential decay.
     use_lr = learning_rates.ExponentialDecay(decay_rate, decay_steps,
@@ -184,6 +191,116 @@ def train():
     train_batch_index += 1
 
   data.exit_gracefully()
+
+def pretrain():
+  """ Pretrains the network on the ILSVRC dataset. """
+  data = data_loader.ImagenetLoader(batch_size, load_batches, image_shape,
+                                    alexnet_cache_dir, alexnet_dataset_files,
+                                    synset_location, synset_list)
+  if not os.path.exists(alexnet_synsets_file):
+    logger.critical("Synset file '%s' not found!" % (alexnet_synsets_file))
+    sys.exit(1)
+  logger.info("Loading synsets file...")
+  data.load(alexnet_synsets_file)
+
+  train = data.get_train_set()
+  test = data.get_test_set()
+  cpu_labels = data.get_non_shared_test_set()[:]
+  train_cpu_labels = data.get_non_shared_train_set()[:]
+
+  # Make the network.
+  logger.info("Building network.")
+
+  conv1 = layers.ConvLayer(kernel_width=7, kernel_height=7, stride_width=2,
+                           stride_height=2, feature_maps=96, border_mode="half")
+  conv2 = layers.ConvLayer(kernel_width=5, kernel_height=5, stride_width=2,
+                           stride_height=2, feature_maps=256, border_mode="half")
+  conv3 = layers.ConvLayer(kernel_width=3, kernel_height=3, feature_maps=512,
+                           border_mode="half")
+
+  pool = layers.Pool(kernel_width=2, kernel_height=2)
+  norm = layers.NormalizationLayer(depth_radius=5, alpha=1e-4, beta=0.75,
+                                   bias=2.0)
+
+  flatten = layers.InnerProductLayer(size=6 * 6 * 512, dropout=True,
+                                     start_bias=1, weight_init="gaussian",
+                                     weight_stddev=0.005)
+  full1 = layers.InnerProductLayer(size=4096, dropout=True, start_bias=1,
+                                   weight_init="gaussian", weight_stddev=0.005)
+  full2 = layers.InnerProductLayer(size=4096, weight_init="gaussian",
+                                   weight_stddev=0.005, dropout=True)
+
+  use_layers = [conv1, norm, pool, conv2, norm, pool, conv3, conv3, conv3, pool,
+                flatten, full1, full2]
+  network = AlexNet(train, test, batch_size, layer_override=use_layers)
+  use_lr = learning_rates.ExponentialDecay(pretrain_decay_rate,
+                                           pretrain_decay_steps,
+                                           learning_rate)
+  network.use_sgd_trainer(use_lr, momentum=momentum, weight_decay=weight_decay)
+
+  logger.info("Starting training...")
+
+  iterations = 0
+  train_batch_index = 0
+  test_batch_index = 0
+
+  while iterations < 320000:
+    logger.debug("Train index, size: %d, %d" % (train_batch_index,
+                                                data.get_train_batch_size()))
+    logger.debug("Test index, size: %d, %d" % (test_batch_index,
+                                               data.get_test_batch_size()))
+
+    # Swap in new data if we need to.
+    if (train_batch_index + 1) * batch_size > data.get_train_batch_size():
+      train_batch_index = 0
+      logger.info("Getting train set.")
+      train = data.get_train_set()
+      train_cpu_labels = data.get_non_shared_train_set()[:]
+      logger.info("Got train set.")
+    # Swap in new data if we need to.
+    test_set_one_patch = data.get_test_batch_size() / 10
+    if (test_batch_index + 1) * batch_size > test_set_one_patch:
+      test_batch_index = 0
+      logger.info("Getting test set.")
+      test = data.get_test_set()
+      cpu_labels = data.get_non_shared_test_set()[:]
+      logger.info("Got test set.")
+
+    if iterations % 100 == 0:
+      # cpu_labels contains labels for every batch currently loaded in VRAM,
+      # without duplicates for additional patches.
+      label_index = test_batch_index * batch_size
+      top_one, top_five = network.test(test_batch_index,
+          cpu_labels[label_index:label_index + batch_size])
+      logger.info("Step %d, testing top 1: %f, testing top 5: %f" % \
+                  (iterations, top_one, top_five))
+
+      # Test it using the training data as well, which we will use to quantify
+      # overfitting.
+      label_index = train_batch_index * batch_size
+      top_one, top_five = \
+          network.test_with_training_data(train_batch_index,
+              train_cpu_labels[label_index:label_index + batch_size])
+      logger.info("(Accuracy on training data: top 1: %f, top 5: %f)" % \
+                  (top_one, top_five))
+
+      test_batch_index += 1
+
+    cost, rate, step = network.train(train_batch_index)
+    logger.info("Training cost: %f, learning rate: %f, step: %d" % \
+                (cost, rate, step))
+
+    if iterations % 100 == 0:
+      logger.info("Saving network...")
+      network.save(save_file)
+      # Save synset data as well.
+      data.save(synsets_save_file)
+
+    iterations += 1
+    train_batch_index += 1
+
+  data.exit_gracefully()
+
 
 def evaluate_final(save_to):
   """ Evaluates the trained network.
@@ -275,6 +392,11 @@ def main():
 
   else:
     logger.info("Running training.")
+
+    if (not os.path.exists(save_file) and not os.path.exists(alexnet_file)):
+      logger.info("No saved networks found, running pre-training...")
+      pretrain()
+
     train()
 
 
